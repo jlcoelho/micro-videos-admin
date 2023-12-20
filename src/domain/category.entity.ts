f765ec2667a36956a6cc91ece6a266a5a54c7a94
João Lucas <lucas_0423@hotmail.com>
2023-12-20 14:04:16 -0300

    feat: add category entity

---
+++ src/domain/category.entity.ts
@@ -0,0 +1,50 @@
+
+export type CategoryConstructorProps = {
+  category_id?: string;
+  name: string;
+  description?: string | null;
+  is_active?: boolean;
+  created_at?: Date;
+}
+
+export type CategoryCreateCommand = {
+  name: string;
+  description?: string | null;
+  is_active?: boolean;
+}
+
+export class Category {
+  category_id: string;
+  name: string;
+  description: string | null;
+  is_active: boolean;
+  created_at: Date;
+
+  constructor(props: CategoryConstructorProps) {
+    this.category_id = props.category_id;
+    this.name = props.name;
+    this.description = props.description ?? null;
+    this.is_active = props.is_active ?? true;
+    this.created_at = props.created_at ?? new Date();
+  }
+
+  static create(props: CategoryCreateCommand) {
+    return new Category(props);
+  }
+
+  changeName(name: string): void {
+    this.name = name;
+  }
+
+  changeDescription(description: string): void {
+    this.description = description;
+  }
+
+  activate() {
+    this.is_active = true;
+  }
+
+  deactivate() {
+    this.is_active = false;
+  }
+}
